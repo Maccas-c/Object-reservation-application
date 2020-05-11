edae5c46db2c94aa25246533c9e74783309aaf2c
Piotr <piotr.ulanicki4@gmail.com>
2020-05-11 19:36:31 +0200

    final version

--- Web-Server/client/src/components/Navigation/Footer/Footer.js
+++ Web-Server/client/src/components/Navigation/Footer/Footer.js
@@ -3,34 +3,34 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Avatar from '../../Avatar/Avatar';
-import Logo from '../../../assets/logo.png';
+import Logo from '../../../assets/logo/logo.png';
 import makeStyles from './FooterStyles';
 
 const Footer = (props) => {
-	const classes = makeStyles();
-	const [value, setValue] = React.useState(0);
-	const modeId = { dark: '0', light: '1' };
+  const classes = makeStyles();
+  const [value, setValue] = React.useState(0);
+  const modeId = { dark: '0', light: '1' };
 
-	const handleChange = (event, newValue) => {
-		setValue(newValue);
-	};
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-	return (
-		<AppBar color='default' className={classes.root}>
-			<Tabs
-				value={value}
-				onChange={handleChange}
-				indicatorColor='primary'
-				textColor='primary'
-				aria-label='full width tabs example'
-				centered
-			>
-				<Tab label='Dark' onClick={() => props.switch(modeId.dark)} />
-				<Tab label='Light' onClick={() => props.switch(modeId.light)} />
-				<Avatar src={Logo} />
-			</Tabs>
-		</AppBar>
-	);
+  return (
+    <AppBar color="default" className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        aria-label="full width tabs example"
+        centered
+      >
+        <Tab label="Dark" onClick={() => props.switch(modeId.dark)} />
+        <Tab label="Light" onClick={() => props.switch(modeId.light)} />
+        <Avatar src={Logo} />
+      </Tabs>
+    </AppBar>
+  );
 };
 
 export default Footer;
