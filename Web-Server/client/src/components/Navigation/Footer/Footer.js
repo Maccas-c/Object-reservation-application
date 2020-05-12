ec5252fa4a3cb435b211125cbde91c3602a1f4f0
Macas <marczakmaciej2@gmail.com>
2020-05-12 16:52:53 +0200

    clean code

--- Web-Server/client/src/components/Navigation/Footer/Footer.js
+++ Web-Server/client/src/components/Navigation/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -7,30 +7,28 @@ import Logo from '../../../assets/logo/logo.png';
 import makeStyles from './FooterStyles';
 
 const Footer = (props) => {
-  const classes = makeStyles();
-  const [value, setValue] = React.useState(0);
-  const modeId = { dark: '0', light: '1' };
+	const classes = makeStyles();
+	const [value, setValue] = useState(0);
+	const modeId = { dark: '0', light: '1' };
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
 
-  return (
-    <AppBar color="default" className={classes.root}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        indicatorColor="primary"
-        textColor="primary"
-        aria-label="full width tabs example"
-        centered
-      >
-        <Tab label="Dark" onClick={() => props.switch(modeId.dark)} />
-        <Tab label="Light" onClick={() => props.switch(modeId.light)} />
-        <Avatar src={Logo} />
-      </Tabs>
-    </AppBar>
-  );
+	return (
+		<AppBar color='default' className={classes.root}>
+			<Tabs
+				value={value}
+				onChange={handleChange}
+				indicatorColor='primary'
+				textColor='primary'
+			>
+				<Tab label='Dark' onClick={() => props.switch(modeId.dark)} />
+				<Tab label='Light' onClick={() => props.switch(modeId.light)} />
+				<Avatar src={Logo} />
+			</Tabs>
+		</AppBar>
+	);
 };
 
 export default Footer;
--- Web-Server/client/src/components/Navigation/SideDrawer/SideDrawer.js
+++ Web-Server/client/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,11 +3,11 @@ import { Link, withRouter } from 'react-router-dom';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import {
-  Drawer,
-  Hidden,
-  List,
-  ListItem,
-  ListItemText
+	Drawer,
+	Hidden,
+	List,
+	ListItem,
+	ListItemText,
 } from '@material-ui/core';
 
 import Avatar from '../../../assets/avatar/avatarMale.png';
@@ -18,91 +18,111 @@ import { MENU_ROUTES } from '../../../constansts/routes/routes';
 import makeStyles from './SideDrawerStyles';
 
 const SideDrawer = (props) => {
-  const classes = makeStyles();
-  const [mobileOpen, setMobileOpen] = useState(false);
-  const {
-    location: { pathname }
-  } = props;
+	const classes = makeStyles();
+	const [mobileOpen, setMobileOpen] = useState(false);
+	const {
+		location: { pathname },
+	} = props;
 
-  const menuList = Object.values(MENU_ITEMS);
+	const menuList = Object.values(MENU_ITEMS);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+	const handleDrawerToggle = () => {
+		setMobileOpen(!mobileOpen);
+	};
 
-  const getKeyByValue = (object, value) => {
-    return Object.keys(object).find((key) => object[key] === value);
-  };
+	const getKeyByValue = (object, value) => {
+		return Object.keys(object).find((key) => object[key] === value);
+	};
 
-  const userRoute = () => {
-    props.history.push(MENU_ROUTES.USER_PROFILE);
-  };
+	const userRoute = () => {
+		props.history.push(MENU_ROUTES.USER_PROFILE);
+	};
 
-  const drawer = (
-    <div className={classes.drawerList}>
-      <List>
-        {menuList.map((item) => {
-          const key = getKeyByValue(MENU_ITEMS, item);
-          return (
-            <ListItem
-              button
-              component={Link}
-              to={MENU_ROUTES[key]}
-              selected={MENU_ROUTES[key] === pathname}
-              key={item}
-            >
-              <ListItemText primary={item} />
-            </ListItem>
-          );
-        })}
-      </List>
-    </div>
-  );
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Header open={handleDrawerToggle} />
+	const drawer = (
+		<div className={classes.drawerList}>
+			<List>
+				{menuList.map((menuitem) => {
+					const key = getKeyByValue(MENU_ITEMS, menuitem);
+					return (
+						<ListItem
+							button
+							component={Link}
+							to={MENU_ROUTES[key]}
+							selected={MENU_ROUTES[key] === pathname}
+							key={menuitem}
+						>
+							<ListItemText primary={menuitem} />
+						</ListItem>
+					);
+				})}
+			</List>
+		</div>
+	);
+	return (
+		<div className={classes.root}>
+			<CssBaseline />
+			<Header open={handleDrawerToggle} />
 
-      <nav className={classes.drawer}>
-        <Hidden smUp>
-          <Drawer
-            variant="temporary"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-          >
-            <div className={classes.userProfile} onClick={userRoute}>
-              <img src={Avatar} alt="Your avatar" height="80" width="80" />
-              <h5 className={classes.userName}>Jan Kowalski</h5>
-              <h5 className={classes.userMail}>example@gmail.com</h5>
-            </div>
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown>
-          <Drawer
-            className={classes.drawer}
-            variant="permanent"
-            classes={{
-              paper: classes.drawerPaper
-            }}
-          >
-            <div className={classes.toolbar} />
-            <div className={classes.userProfile} onClick={userRoute}>
-              <img src={Avatar} alt="Your avatar" height="80" width="80" />
-              <h5 className={classes.userName}>Jan Kowalski</h5>
-              <h5 className={classes.userMail}>example@gmail.com</h5>
-            </div>
-            {drawer}
-          </Drawer>
-        </Hidden>
-      </nav>
+			<nav className={classes.drawer}>
+				<Hidden smUp>
+					<Drawer
+						variant='temporary'
+						open={mobileOpen}
+						onClose={handleDrawerToggle}
+					>
+						<div
+							className={classes.userProfile}
+							onClick={userRoute}
+						>
+							<img
+								src={Avatar}
+								alt='Your avatar'
+								height='80'
+								width='80'
+							/>
+							<h5 className={classes.userName}>Jan Kowalski</h5>
+							<h5 className={classes.userMail}>
+								example@gmail.com
+							</h5>
+						</div>
+						{drawer}
+					</Drawer>
+				</Hidden>
+				<Hidden xsDown>
+					<Drawer
+						className={classes.drawer}
+						variant='permanent'
+						classes={{
+							paper: classes.drawerPaper,
+						}}
+					>
+						<div className={classes.toolbar} />
+						<div
+							className={classes.userProfile}
+							onClick={userRoute}
+						>
+							<img
+								src={Avatar}
+								alt='Your avatar'
+								height='80'
+								width='80'
+							/>
+							<h5 className={classes.userName}>Jan Kowalski</h5>
+							<h5 className={classes.userMail}>
+								example@gmail.com
+							</h5>
+						</div>
+						{drawer}
+					</Drawer>
+				</Hidden>
+			</nav>
 
-      <div className={classes.content}>
-        <div className={classes.toolbar} />
-        {props.children}
-      </div>
-    </div>
-  );
+			<div className={classes.content}>
+				<div className={classes.toolbar} />
+				{props.children}
+			</div>
+		</div>
+	);
 };
 
 export default withRouter(SideDrawer);
