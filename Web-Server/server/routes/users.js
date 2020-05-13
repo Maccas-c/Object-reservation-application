9cc4940594fcad16a6a8ac1c0b87662ea2f95be8
naapster <michalkubiak37@gmail.com>
2020-05-13 11:06:53 +0200

    validation email and password

--- Web-Server/server/routes/users.js
+++ Web-Server/server/routes/users.js
@@ -1,7 +1,11 @@
-var express = require("express");
+const express = require("express");
 const userModel = require("../models/userModel");
 const genPassword = require("../lib/password").genPassword;
-var router = express.Router();
+const {
+  check,
+  validationResult
+} = require("express-validator");
+const router = express.Router();
 
 router.get("/user", function (req, res, next) {
   res.send("respond with a resource");
@@ -16,51 +20,64 @@ router.get("/users", async (req, res) => {
   }
 });
 
-router.post("/user/create", async (req, res) => {
-  const isExist = userModel.findOne(
-    {
-      "login.email": req.body.email,
-    },
-    async function (err, user) {
-      if (err) return res.status(404).json(err);
-      if (user) return res.status(422).json("The email exist");
-      else {
-        const saltHash = await genPassword(req.body.password);
-        const salt = saltHash.salt;
-        const hash = saltHash.hash;
+router.post("/user/create",
+  [check("email").
+    isEmail().
+    notEmpty(),
 
-        const user = new userModel({
-          login: {
-            email: req.body.email,
-            hash: hash,
-            salt: salt,
-          },
-          name: req.body.name,
-          surname: req.body.surname,
-        });
-        try {
-          const savedUser = await user.save();
-          res.status(201).json(savedUser);
-        } catch (err) {
-          res.status(400).res.json(err);
+    check('password')
+    .isLength(5)
+    .notEmpty()
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/)
+    .withMessage('Password should be combination of one uppercase , one lower case, one digit and min 6 , max 20 char long'),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array()
+      });
+    }
+    const isExist = userModel.findOne({
+        "login.email": req.body.email,
+      },
+      async function (err, user) {
+        if (err) return res.status(404).json(err);
+        if (user) return res.status(422).json("The email exist");
+        else {
+          const saltHash = await genPassword(req.body.password);
+          const salt = saltHash.salt;
+          const hash = saltHash.hash;
+
+          const user = new userModel({
+            login: {
+              email: req.body.email,
+              hash: hash,
+              salt: salt,
+            },
+            // name: req.body.name,
+            // surname: req.body.surname,
+          });
+          try {
+            const savedUser = await user.save();
+            res.status(201).json(savedUser);
+          } catch (err) {
+            res.status(400).res.json(err);
+          }
         }
       }
-    }
-  );
-});
+    );
+  });
 
 router.patch("/user/delete/:userId", async (req, res) => {
   try {
-    const deletedUser = await userModel.updateOne(
-      {
-        _id: req.params.userId,
+    const deletedUser = await userModel.updateOne({
+      _id: req.params.userId,
+    }, {
+      $set: {
+        isActive: false,
       },
-      {
-        $set: {
-          isActive: false,
-        },
-      }
-    );
+    });
     res.status(200).json(deletedUser);
   } catch (err) {
     res.status(404).json(err);
@@ -69,28 +86,25 @@ router.patch("/user/delete/:userId", async (req, res) => {
 
 router.patch("/user/update/:userId", async (req, res) => {
   try {
-    const updatedUser = await userModel.updateOne(
-      {
-        _id: req.params.userId,
-      },
-      {
-        $set: {
-          name: req.body.name,
-          surname: req.body.surname,
-          age: req.body.age,
-          phone_number: req.body.phone_number,
-          address: {
-            street: req.body.street,
-            city: req.body.city,
-            postalCode: req.body.postalCode,
-          },
-          vat: {
-            nip: req.body.nip,
-            regon: req.body.regon,
-          },
+    const updatedUser = await userModel.updateOne({
+      _id: req.params.userId,
+    }, {
+      $set: {
+        name: req.body.name,
+        surname: req.body.surname,
+        age: req.body.age,
+        phone_number: req.body.phone_number,
+        address: {
+          street: req.body.street,
+          city: req.body.city,
+          postalCode: req.body.postalCode,
         },
-      }
-    );
+        vat: {
+          nip: req.body.nip,
+          regon: req.body.regon,
+        },
+      },
+    });
     res.status(200).json(updatedUser);
   } catch (err) {
     res.status(404).json(err);
