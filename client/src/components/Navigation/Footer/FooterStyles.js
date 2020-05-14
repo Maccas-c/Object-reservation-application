30706c94376ca0bfb571c5b388b2c9fa0144511e
Macas <marczakmaciej2@gmail.com>
2020-05-14 19:58:24 +0200

    test

--- client/src/components/Navigation/Footer/FooterStyles.js
+++ client/src/components/Navigation/Footer/FooterStyles.js
@@ -5,10 +5,10 @@ const useStyles = makeStyles((theme) => ({
 		zIndex: theme.zIndex.drawer + 1,
 		width: '100%',
 		height: '50px',
-		overflow: 'auto',
-		position: 'fixed',
+		position: 'absolute',
 		marginTop: 'calc(100vh - 50px)',
-		paddingBottom: '60px',
+		bottom: 0,
+		left: 0,
 	},
 }));
 
