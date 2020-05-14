ef473ba2307ed25939910961115c2df02741e60f
Piotr <piotr.ulanicki4@gmail.com>
2020-05-15 01:04:56 +0200

    new resp

--- client/src/components/Navigation/Footer/FooterStyles.js
+++ client/src/components/Navigation/Footer/FooterStyles.js
@@ -1,14 +1,14 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		zIndex: theme.zIndex.drawer + 1,
-		width: '100%',
-		height: '50px',
-		position: '-webkit-sticky',
-		marginTop: 'calc(100vh - 50px)',
-		bottom: 0,
-	},
+  root: {
+    zIndex: theme.zIndex.drawer + 1,
+    width: '100%',
+    height: '50px',
+    position: 'sticky',
+    marginTop: 'calc(100vh - 50px)',
+    bottom: 0
+  }
 }));
 
 export default useStyles;
