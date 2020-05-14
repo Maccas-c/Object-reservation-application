10dbec91dba3c0b079d4c1d6d50d9ebe0c6942f0
Macas <marczakmaciej2@gmail.com>
2020-05-14 18:40:17 +0200

    Test Sticky Footer

--- client/src/components/Navigation/Footer/FooterStyles.js
+++ client/src/components/Navigation/Footer/FooterStyles.js
@@ -3,9 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
 	root: {
 		zIndex: theme.zIndex.drawer + 1,
-		width: '100%',
+		width: 'auto',
 		height: '50px',
-		position: 'absolute',
+		position: 'sticky',
 		marginTop: 'calc(100vh - 50px)',
 		bottom: 0,
 	},
