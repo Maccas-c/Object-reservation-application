58c76472ad7e8d1d354dd06474f5408d22b1b219
Macas <marczakmaciej2@gmail.com>
2020-05-14 19:11:04 +0200

    Hotfix footer

--- client/src/components/Navigation/Footer/FooterStyles.js
+++ client/src/components/Navigation/Footer/FooterStyles.js
@@ -3,9 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
 	root: {
 		zIndex: theme.zIndex.drawer + 1,
-		width: 'auto',
+		width: '100%',
 		height: '50px',
-		position: 'sticky',
+		position: 'fixed',
 		marginTop: 'calc(100vh - 50px)',
 		bottom: 0,
 	},
