3bce5b404680379df7dd7095ca8d381a999b65ad
Macas <marczakmaciej2@gmail.com>
2020-05-14 21:31:37 +0200

    test

--- client/src/components/Navigation/Footer/FooterStyles.js
+++ client/src/components/Navigation/Footer/FooterStyles.js
@@ -4,7 +4,6 @@ const useStyles = makeStyles((theme) => ({
 	root: {
 		zIndex: theme.zIndex.drawer + 1,
 		width: '100%',
-		maxheight: '50px',
 		position: 'absolute',
 		marginTop: 'calc(100vh - 50px)',
 		bottom: 0,
