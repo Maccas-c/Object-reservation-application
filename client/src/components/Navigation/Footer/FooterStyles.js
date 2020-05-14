3a8102769fc8347813c7a371d8a5ddb0e3df2072
Macas <marczakmaciej2@gmail.com>
2020-05-14 21:35:23 +0200

    fix too long page

--- client/src/components/Navigation/Footer/FooterStyles.js
+++ client/src/components/Navigation/Footer/FooterStyles.js
@@ -4,7 +4,8 @@ const useStyles = makeStyles((theme) => ({
 	root: {
 		zIndex: theme.zIndex.drawer + 1,
 		width: '100%',
-		position: 'absolute',
+		height: '50px',
+		position: 'sticky',
 		marginTop: 'calc(100vh - 50px)',
 		bottom: 0,
 	},
