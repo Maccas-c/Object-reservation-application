df658fa518ff2ec8ca7de2e821f2ad324a4dee8a
Macas <marczakmaciej2@gmail.com>
2020-05-14 19:22:08 +0200

    Test paddingBottom

--- client/src/components/Navigation/Footer/FooterStyles.js
+++ client/src/components/Navigation/Footer/FooterStyles.js
@@ -5,9 +5,10 @@ const useStyles = makeStyles((theme) => ({
 		zIndex: theme.zIndex.drawer + 1,
 		width: '100%',
 		height: '50px',
+		overflow: 'auto',
 		position: 'fixed',
 		marginTop: 'calc(100vh - 50px)',
-		bottom: 0,
+		paddingBottom: '60px',
 	},
 }));
 
