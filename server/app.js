e54ab51f4e84dcc0d56e1016edf4b343d577bbb8
naapster <michalkubiak37@gmail.com>
2020-05-15 11:50:42 +0200

    dla Pitera

--- server/app.js
+++ server/app.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const mongoStore = require('connect-mongo')(session);
-const passport = require('passport');
-require('dotenv').config();
-require('./config/passport');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const reservationRouter = require('./routes/reservations');
-const courtRouter = require('./routes/courts');
-const loginUsosRouter = require('./routes/loginUsos');
-const login = require('./routes/login');
-const connection = require('./config/database');
-const crypto = require('crypto');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const mongoStore = require("connect-mongo")(session);
+const passport = require("passport");
+require("dotenv").config();
+require("./config/passport");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const reservationRouter = require("./routes/reservations");
+const courtRouter = require("./routes/courts");
+const loginUsosRouter = require("./routes/loginUsos");
+const login = require("./routes/login");
+const connection = require("./config/database");
+const crypto = require("crypto");
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use(logger('dev'));
+app.use(cors({
+  credentials: true,
+  origin: "http://localhost:3000"
+}));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -30,10 +33,9 @@ app.use(
   })
 );
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(
   session({
@@ -41,9 +43,9 @@ app.use(
     store: new mongoStore({
       mongooseConnection: mongoose.connection,
       ttl: 5 * 60,
-      autoRemove: 'native'
+      autoRemove: "native"
     }),
-    dbName: 'DevelopTeam',
+    dbName: "DevelopTeam",
     resave: false,
     saveUninitialized: false
   })
@@ -61,8 +63,8 @@ app.use(login);
 //   res.sendFile(path.join(__dirname + '../client/build/index.html'));
 // });
 
-app.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// app.get('*', function (request, response) {
+//   response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
 
 module.exports = app;
