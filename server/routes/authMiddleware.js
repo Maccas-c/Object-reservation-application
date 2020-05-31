9327c7d5bc0aa7267b9a36fc60541f67bf37f84b
Przemysław Owczarczyk <owca.przemek@hotmail.com>
2020-05-31 21:17:46 +0200

    email update middleware

--- server/routes/authMiddleware.js
+++ server/routes/authMiddleware.js
@@ -1,10 +1,12 @@
+const userModel = require("./../models/userModel");
+
 module.exports.isAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
     //req.isAuthenticated() will return true if user is logged in
     next();
   } else {
     res.status(401).json({
-      msg: 'You are not authorized to view this resource'
+      msg: "You are not authorized to view this resource",
     });
   }
 };
@@ -13,7 +15,7 @@ module.exports.authRole = (role) => {
   return (req, res, next) => {
     if (req.user.role !== role) {
       res.status(401);
-      return res.send('Not allowed');
+      return res.send("Not allowed");
     }
     next();
   };
@@ -24,7 +26,17 @@ module.exports.checkUser = (req, res, next) => {
     next();
   } else {
     res.status(401).json({
-      msg: 'You are diffrent user'
+      msg: "You are diffrent user",
     });
   }
+};
+module.exports.checkEmail = (req, res, next) => {
+  const isExist = userModel.findOne({ email: req.body.email }, async function (
+    err,
+    user
+  ) {
+    if (err) return res.status(404).json(err);
+    else if (user) return res.status(422).json("The email exist");
+    else next();
+  });
 };
