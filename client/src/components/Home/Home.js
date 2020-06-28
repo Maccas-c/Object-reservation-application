5e154c6dd52f6e29d224868f76f12d3aca82cc41
Macas <35805288+MaciejM123@users.noreply.github.com>
2020-06-29 00:23:28 +0200

    add court

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,13 +1,27 @@
-import React, {
-    Fragment
-} from 'react';
-
+import React, { Fragment } from 'react';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Image from '../../assets/court/Boisko.png';
 const Home = () => {
-    return ( <
-        Fragment >
-        <
-        div > Home < /div> <
-        /Fragment>
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            display: 'flex',
+            alignItems: 'center',
+            marginLeft: '20px',
+            margin: theme.spacing(1),
+            width: theme.spacing(144),
+            height: theme.spacing(80),
+            backgroundImage: `url(${Image})`,
+        },
+    }));
+    const classes = useStyles();
+
+    return (
+        <Fragment>
+            <div className={classes.root}>
+                <Paper elevation={3}></Paper>
+            </div>
+        </Fragment>
     );
 };
 
