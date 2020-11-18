56d3ad27de79701ad8cc83732e73e310b2da2482
owczarczykk <owca.przemek@hotmail.com>
2020-11-18 16:37:42 +0100

    add shop

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -7,16 +7,35 @@ router.post('/getToken', function (req, res) {
   request(
     {
       method: 'POST',
-      url: 'https://secure.payu.com/pl/standard/user/oauth/authorize',
+      url: 'https://secure.snd.payu.com/pl/standard/user/oauth/authorize',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       body:
-        'grant_type=client_credentials&client_id=145227&client_secret=12f071174cb7eb79d4aac5bc2f07563f',
+        'grant_type=client_credentials&client_id=399358&client_secret=23c6ab603340ce6c2d588a76989197b0',
     },
     function (error, response, body) {
       let jsonbody = JSON.parse(body);
       console.log(jsonbody);
+      let a = 'Bearer ' + jsonbody.access_token;
+      console.log(a);
+      request(
+        {
+          method: 'POST',
+          url: 'https://secure.snd.payu.com/api/v2_1/orders/',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: a,
+          },
+          body:
+            '{  "notifyUrl": "https://localhost:3001/notify",  "customerIp": "127.0.0.1",  "merchantPosId": "399358",  "description": "RTV market",  "currencyCode": "PLN",  "totalAmount": "21000",  "products": [    {      "name": "Wireless mouse",      "unitPrice": "15000",      "quantity": "1"    },    {      "name": "HDMI cable",      "unitPrice": "6000",      "quantity": "1"    }  ]}',
+        },
+        function (error2, response2, body2) {
+          console.log('Status:', response2.statusCode);
+          console.log('Headers:', JSON.stringify(response2.headers));
+          console.log('Response:', body2);
+        },
+      );
     },
   );
 });
@@ -24,13 +43,13 @@ router.post('/createPayment', function (req, res) {
   request(
     {
       method: 'POST',
-      url: 'https://secure.payu.com/api/v2_1/orders/',
+      url: 'https://secure.snd.payu.com/api/v2_1/orders/',
       headers: {
         'Content-Type': 'application/json',
         Authorization: 'Bearer 3e5cac39-7e38-4139-8fd6-30adc06a61bd',
       },
       body:
-        '{  "notifyUrl": "https://your.eshop.com/notify",  "customerIp": "127.0.0.1",  "merchantPosId": "145227",  "description": "RTV market",  "currencyCode": "PLN",  "totalAmount": "21000",  "products": [    {      "name": "Wireless mouse",      "unitPrice": "15000",      "quantity": "1"    },    {      "name": "HDMI cable",      "unitPrice": "6000",      "quantity": "1"    }  ]}',
+        '{  "notifyUrl": "https://localhost:3001/notify",  "customerIp": "127.0.0.1",  "merchantPosId": "399358",  "description": "RTV market",  "currencyCode": "PLN",  "totalAmount": "21000",  "products": [    {      "name": "Wireless mouse",      "unitPrice": "15000",      "quantity": "1"    },    {      "name": "HDMI cable",      "unitPrice": "6000",      "quantity": "1"    }  ]}',
     },
     function (error, response, body) {
       console.log('Status:', response.statusCode);
@@ -40,11 +59,11 @@ router.post('/createPayment', function (req, res) {
   );
 });
 
-router.post('/payments', function (req, res) {
+router.get('/payments', function (req, res) {
   request(
     {
       method: 'GET',
-      url: 'https://secure.payu.com/api/v2_1/paymethods/',
+      url: 'https://secure.snd.payu.com/api/v2_1/paymethods/',
       headers: {
         Authorization: 'Bearer 3e5cac39-7e38-4139-8fd6-30adc06a61bd',
       },
@@ -52,7 +71,24 @@ router.post('/payments', function (req, res) {
     function (error, response, body) {
       console.log('Status:', response.statusCode);
       console.log('Headers:', JSON.stringify(response.headers));
-      console.log('Response:', body);
+      let jsonbody = JSON.parse(body);
+      console.log(jsonbody);
+    },
+  );
+});
+
+router.get('/notify', function (req, res) {
+  request(
+    {
+      method: 'POST',
+      url: 'https://localhost:3001/notify',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+    function (error, response, body) {
+      let jsonbody = JSON.parse(body);
+      console.log(jsonbody);
     },
   );
 });
