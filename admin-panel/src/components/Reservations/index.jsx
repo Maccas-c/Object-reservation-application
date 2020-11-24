48762847149f6ac97bcfb14972e39643d374616f
Macas <marczakmaciej2@gmail.com>
2020-11-24 18:09:11 +0100

    delete show ReservationShow.jsx

--- admin-panel/src/components/Reservations/index.jsx
+++ admin-panel/src/components/Reservations/index.jsx
@@ -1,7 +1,5 @@
 import {ReservationsList} from './ReservationsList';
-import ReservationShow from "./ReservationShow";
 
 export default {
     list: ReservationsList,
-    show: ReservationShow,
 };
