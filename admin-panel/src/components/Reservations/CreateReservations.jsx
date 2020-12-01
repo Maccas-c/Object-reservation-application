5283261fc094745cbd13b79f0dfceb215ae0b8cb
Macas <marczakmaciej2@gmail.com>
2020-12-01 20:28:45 +0100

    mozliwosc dodania rezerwacji

--- admin-panel/src/components/Reservations/CreateReservations.jsx
+++ admin-panel/src/components/Reservations/CreateReservations.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 export const CreateReservations = (props) => (
   <Create {...props}>
     <SimpleForm>
-      <DateInput label={'Dzień'} source={'hour'} />
-      <TextInput label={'Godzina'} source={'start_time'} />
+      <DateInput label={'Dzień'} source={'start_time'} />
+      <TextInput label={'Godzina'} source={'hour'} defaultValue={'15:00'} />
       <TextInput label={'Strefa Boiska'} source={'courtid'} />
       <TextInput label={'Imię'} source={'userid.name'} />
       <TextInput label={'Nazwisko'} source={'userid.surname'} />
