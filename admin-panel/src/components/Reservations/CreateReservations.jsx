d595e0af169ac2b9a17dea4c883a8fab0b53cbd9
Macas <marczakmaciej2@gmail.com>
2020-12-03 20:13:33 +0100

    Dodawanie rezerwacji pojedynczych dla admina

--- admin-panel/src/components/Reservations/CreateReservations.jsx
+++ admin-panel/src/components/Reservations/CreateReservations.jsx
@@ -1,14 +1,15 @@
 import { Create, DateInput, SimpleForm, TextInput } from 'react-admin';
 import React from 'react';
 
-export const CreateReservations = (props) => (
+const userId = localStorage.getItem('user').slice(8, 32);
+
+export const CreateReservations = ({ ...props }) => (
   <Create {...props}>
     <SimpleForm>
-      <DateInput label={'Dzień'} source={'start_time'} />
-      <TextInput label={'Godzina'} source={'hour'} defaultValue={'15:00'} />
-      <TextInput label={'Strefa Boiska'} source={'courtid'} />
-      <TextInput label={'Imię'} source={'userid.name'} />
-      <TextInput label={'Nazwisko'} source={'userid.surname'} />
+      <DateInput label={'Dzień'} source={'start_time'} required />
+      <TextInput label={'Godzina'} source={'hour'} defaultValue={'15:00'} required />
+      <TextInput label={'Strefa Boiska'} source={'courtId'} required />
+      <TextInput label={'Id'} source={'userId'} defaultValue={`${userId}`} disabled />
     </SimpleForm>
   </Create>
 );
