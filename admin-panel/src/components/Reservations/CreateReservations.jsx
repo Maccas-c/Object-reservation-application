e8bf4c96e0d2d0159452c95ccca521d22dfd0ccb
Macas <marczakmaciej2@gmail.com>
2020-12-06 18:27:15 +0100

    userId path w tworzeniu rezerwacji admin-panel-court-newVersion

--- admin-panel/src/components/Reservations/CreateReservations.jsx
+++ admin-panel/src/components/Reservations/CreateReservations.jsx
@@ -9,7 +9,7 @@ export const CreateReservations = ({ ...props }) => (
       <DateInput label={'Dzień'} source={'start_time'} required />
       <TextInput label={'Godzina'} source={'hour'} defaultValue={'15:00'} required />
       <TextInput label={'Strefa Boiska'} source={'courtId'} required />
-      <TextInput label={'Id'} source={'_id'} defaultValue={`${_id}`} disabled />
+      <TextInput label={'Id'} source={'userId'} defaultValue={`${_id}`} disabled />
     </SimpleForm>
   </Create>
 );
