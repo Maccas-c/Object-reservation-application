7c2f808392d693016ef40aa42834a67dd7d88820
owczarczykk <owca.przemek@hotmail.com>
2020-12-09 17:37:06 +0100

    fix update tariff

--- server/controllers/tariffController.js
+++ server/controllers/tariffController.js
@@ -1,5 +1,5 @@
-const tariffModel = require("../models/tariffModel");
-const courtsTariff = require("../models/tariffModel");
+const tariffModel = require('../models/tariffModel');
+const courtsTariff = require('../models/tariffModel');
 
 module.exports.tariffCreate = async function (req, res) {
   const courts = new courtsTariff({
@@ -20,7 +20,7 @@ module.exports.tariffsGet = async function (req, res) {
   try {
     const tariff = await courtsTariff.find();
     const tariffFixed = JSON.parse(
-      JSON.stringify(tariff).split('"_id":').join('"id":')
+      JSON.stringify(tariff).split('"_id":').join('"id":'),
     );
     res.status(200).json(tariffFixed);
   } catch (err) {
@@ -31,7 +31,7 @@ module.exports.tariffGet = async function (req, res) {
   try {
     const tariff = await courtsTariff.findById(req.params.id);
     const tariffFixed = JSON.parse(
-      JSON.stringify(tariff).split('"_id":').join('"id":')
+      JSON.stringify(tariff).split('"_id":').join('"id":'),
     );
     res.status(200).json(tariffFixed);
   } catch (err) {
@@ -41,13 +41,22 @@ module.exports.tariffGet = async function (req, res) {
 
 module.exports.tariffUpdate = async function (req, res) {
   try {
+    const resposneData = req.body;
     const tariffUpdate = await tariffModel.updateOne(
       {
         _id: req.params.id,
       },
-      req.body
+      {
+        $set: {
+          ids: req.body.ids,
+          name: req.body.name,
+          classes_and_sports_training: req.body.classes_and_sports_training,
+          tournament_matches: req.body.tournament_matches,
+          university_club: req.body.university_club,
+        },
+      },
     );
-    res.status(200).json(tariffUpdate);
+    res.status(200).send(resposneData);
   } catch (err) {
     res.status(404).json(err);
   }
