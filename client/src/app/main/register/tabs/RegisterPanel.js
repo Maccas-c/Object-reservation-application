4a48cbb4ac86d276c1cdda7a188f58f2414fc330
Macas <marczakmaciej2@gmail.com>
2021-01-10 19:00:08 +0100

    rejestracja

---
+++ client/src/app/main/register/tabs/RegisterPanel.js
@@ -0,0 +1,192 @@
+import { RadioGroupFormsy, TextFieldFormsy } from '@fuse/core/formsy';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Formsy from 'formsy-react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { registerError, submitRegister } from 'app/auth/store/registerSlice';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import { useHistory } from 'react-router';
+import ValidationSchema from './ValidationSchema';
+
+function RegisterPanel(props) {
+	const dispatch = useDispatch();
+	const register = useSelector(({ auth }) => auth.register);
+
+	const [isFormValid, setIsFormValid] = useState(false);
+	const formRef = useRef(null);
+
+	const history = useHistory();
+
+	useEffect(() => {
+		if (register.error && (register.error.username || register.error.password || register.error.email)) {
+			formRef.current.updateInputsWithError({
+				...register.error
+			});
+			disableButton();
+		}
+		if (register.success) {
+			dispatch(registerError());
+			history.push('/login');
+		}
+	}, [register]);
+
+	function disableButton() {
+		setIsFormValid(false);
+	}
+
+	function enableButton() {
+		setIsFormValid(true);
+	}
+
+	function handleSubmit(model) {
+		dispatch(submitRegister(model));
+	}
+
+	return (
+		<div className="w-full">
+			<Formsy
+				onValidSubmit={handleSubmit}
+				onValid={enableButton}
+				onInvalid={disableButton}
+				ref={formRef}
+				className="flex flex-col justify-center w-full"
+			>
+				<TextFieldFormsy
+					className="mb-16"
+					type="text"
+					name="name"
+					label="Imię"
+					validations={{
+						minLength: 4
+					}}
+					validationErrors={{
+						minLength: 'Min character length is 4'
+					}}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="end">
+								<Icon className="text-20" color="action">
+									person
+								</Icon>
+							</InputAdornment>
+						)
+					}}
+					variant="outlined"
+					required
+				/>
+				<TextFieldFormsy
+					className="mb-16"
+					type="text"
+					name="surname"
+					label="Nazwisko"
+					validations={{
+						minLength: 4
+					}}
+					validationErrors={{
+						minLength: 'Min character length is 4'
+					}}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="end">
+								<Icon className="text-20" color="action">
+									person
+								</Icon>
+							</InputAdornment>
+						)
+					}}
+					variant="outlined"
+					required
+				/>
+
+				<TextFieldFormsy
+					className="mb-16"
+					type="text"
+					name="email"
+					label="Email"
+					validations="isEmail"
+					validationErrors={{
+						isEmail: 'Wpisz poprawny email !'
+					}}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="end">
+								<Icon className="text-20" color="action">
+									email
+								</Icon>
+							</InputAdornment>
+						)
+					}}
+					variant="outlined"
+					required
+				/>
+
+				<TextFieldFormsy
+					className="mb-16"
+					type="password"
+					name="password"
+					label="Hasło"
+					validations={ValidationSchema}
+					validationErrors={{
+						password:
+							"    'Hasło musi się składać z co najmniej 6 i co najwyżej 20 znaków. Prawidłowe hasło musi zawierać co najmniej jedną małą literę, co najmniej jedna duża literę, jeden znak specjalny oraz jedną cyfrę.',\n",
+						equalsField: 'Hasło nie jest takie samo.'
+					}}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="end">
+								<Icon className="text-20" color="action">
+									vpn_key
+								</Icon>
+							</InputAdornment>
+						)
+					}}
+					variant="outlined"
+					required
+				/>
+
+				<TextFieldFormsy
+					className="mb-16"
+					type="password"
+					name="password-confirm"
+					label="Potwierdzenie hasła"
+					validations="equalsField:password"
+					validationErrors={{
+						equalsField: 'Hasło nie jest takie samo.'
+					}}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="end">
+								<Icon className="text-20" color="action">
+									vpn_key
+								</Icon>
+							</InputAdornment>
+						)
+					}}
+					variant="outlined"
+					required
+				/>
+				<RadioGroupFormsy aria-label="gender" name="sex" defaultValue="female">
+					<FormControlLabel value="female" name="female" control={<Radio />} label="Kobieta" />
+					<FormControlLabel value="male" name="male" control={<Radio />} label="Mężczyzna" />
+				</RadioGroupFormsy>
+
+				<Button
+					type="submit"
+					variant="contained"
+					color="primary"
+					className="w-full mx-auto mt-16 normal-case"
+					aria-label="REGISTER"
+					disabled={!isFormValid}
+					value="legacy"
+				>
+					Rejestruj
+				</Button>
+			</Formsy>
+		</div>
+	);
+}
+
+export default RegisterPanel;
