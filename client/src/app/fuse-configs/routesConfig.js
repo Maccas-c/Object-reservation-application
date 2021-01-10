de8ca78dd41b01218abce6cb310c947a2836dc92
Macas <marczakmaciej2@gmail.com>
2021-01-10 17:09:33 +0100

    rejestracja

--- client/src/app/fuse-configs/routesConfig.js
+++ client/src/app/fuse-configs/routesConfig.js
@@ -5,8 +5,9 @@ import LoginConfig from 'app/main/login/LoginConfig';
 import pagesConfigs from 'app/main/pages/pagesConfigs';
 
 import FuseUtils from '@fuse/utils';
+import RegisterConfig from '../main/register/RegisterConfig';
 
-const routeConfigs = [...pagesConfigs, LoginConfig];
+const routeConfigs = [...pagesConfigs, LoginConfig, RegisterConfig];
 
 const routes = [
 	...FuseUtils.generateRoutesFromConfigs(routeConfigs),
