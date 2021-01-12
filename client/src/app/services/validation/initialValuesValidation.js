178f0174abe58b9d2131cbd841948cff3057617c
Macas <marczakmaciej2@gmail.com>
2021-01-12 17:25:52 +0100

    updateUser

---
+++ client/src/app/services/validation/initialValuesValidation.js
@@ -0,0 +1,27 @@
+export const userProfileTransform = apiData => ({
+	id: apiData._id,
+	name: apiData.name,
+	surname: apiData.surname,
+	email: apiData.email,
+	phone_number: apiData.phone_number,
+	age: apiData.age,
+	adress_city: apiData.adress_city,
+	adress_postalCode: apiData.adress_postalCode,
+	adress_street: apiData.adress_street,
+	sex: apiData.sex,
+	nip: apiData.nip
+});
+
+export const initialValuesUserProfile = () => ({
+	id: '',
+	name: '',
+	surname: '',
+	email: '',
+	phone_number: '',
+	age: '',
+	adress_city: '',
+	adress_postalCode: '',
+	adress_street: '',
+	sex: '',
+	nip: ''
+});
