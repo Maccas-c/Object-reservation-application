cbb6d829862e34fc4e8163ed86f8499d04f6d985
Macas <marczakmaciej2@gmail.com>
2021-01-12 23:22:40 +0100

    Potwierdzenie załozenia konta

---
+++ client/src/app/main/mailConfirm/MailConfirmConfig.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { authRoles } from '../../auth';
+import MailConfirm from './MailConfirm';
+
+const MailConfirmConfig = {
+	settings: {
+		layout: {
+			config: {
+				navbar: {
+					display: false
+				},
+				toolbar: {
+					display: false
+				},
+				footer: {
+					display: false
+				},
+				leftSidePanel: {
+					display: false
+				},
+				rightSidePanel: {
+					display: false
+				}
+			}
+		}
+	},
+	auth: authRoles.onlyGuest,
+	routes: [
+		{
+			path: '/mailConfirm',
+			component: MailConfirm
+		}
+	]
+};
+
+export default MailConfirmConfig;
