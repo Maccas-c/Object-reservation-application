5e3dc77410bd834e8ccd8e6e39edea6c8aa93181
Macas <marczakmaciej2@gmail.com>
2021-01-13 22:39:09 +0100

    walidacja rejestracja

--- client/src/app/services/validation/validationSchema.js
+++ client/src/app/services/validation/validationSchema.js
@@ -12,16 +12,22 @@ export const registerSchema = Yup.object().shape({
 export const userProfileEdit = Yup.object().shape({
 	nip: Yup.string().matches(
 		/^((\d{3}[-]\d{3}[-]\d{2}[-]\d{2})|(\d{3}[-]\d{2}[-]\d{2}[-]\d{3}))$/,
-		'Niepoprawny format NIP'
+		'Niepoprawny format NIP, przykładowy format: XXX-XX-XX-XXX lub XXX-XX-XX-XXX'
 	),
-	adress_postalCode: Yup.string().matches(/^\d{2}[- ]{0,1}\d{3}$/, 'Kod pocztowy powinien być w postaci XX-XXX'),
+	adress_postalCode: Yup.string().matches(
+		/^\d{2}[- ]{0,1}\d{3}$/,
+		'	Niepoprawny format kodu pocztowego, przykładowy format: XX-XXX'
+	),
 	phone_number: Yup.string().matches(
 		/(?<!\w)(\(?(\+|00)?48\)?)?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}(?!\w)/,
 		'Niepoprawny format numeru!'
 	),
-	age: Yup.string().matches(/^([1-9][0-9]?){0,1}$/, 'Niepoprawny wiek'),
+	age: Yup.string().matches(
+		/^([1-9][0-9]?){0,1}$/,
+		'Podany wiek jest błędny, wiek musi być z zakresu od 0 do 99 lat '
+	),
 	email: Yup.string().matches(
-		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-		'Niepoprawny mail'
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+		'Niepoprawny format e-mail'
 	)
 });
