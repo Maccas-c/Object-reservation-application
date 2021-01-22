8d24ff2cc7908f92181efdf31856c807c6afe857
Macas <marczakmaciej2@gmail.com>
2021-01-22 16:49:16 +0100

    delete select in PriceList.jsx

--- admin-panel/src/components/PriceList/PriceList.jsx
+++ admin-panel/src/components/PriceList/PriceList.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { Datagrid, EditButton, List, Responsive, TextField } from 'react-admin';
 
 export const PriceList = ({ ...props }) => (
-  <List {...props} title={'Cennik'} exporter={false}>
+  <List bulkActionButtons={false} {...props} title={'Cennik'} exporter={false}>
     <Responsive
       medium={
-        <Datagrid unselectable={'off'} title={'Rodzaj obiektu sportowego '} size={'medium'}>
+        <Datagrid isRowSelectable={() => false} title={'Rodzaj obiektu sportowego '} size={'medium'}>
           <TextField label={'Strefa'} source={'name'} />
           <TextField label={'Zajecia Rekrutacyjno Sportowe Treningi'} source={'classes_and_sports_training'} />
           <TextField label={'Mecze Turniejowe'} source={'tournament_matches'} />
