5eda230586db012bf54bec07c2a5781a9e194f3f
Macas <marczakmaciej2@gmail.com>
2021-01-22 19:01:58 +0100

    court create

--- admin-panel/src/components/Court/CourtCreate.jsx
+++ admin-panel/src/components/Court/CourtCreate.jsx
@@ -26,6 +26,7 @@ export const CourtCreate = (props) => (
       <SelectInput label={'Strefa'} source={'nameCourt'} choices={zones} />
       <SelectInput label={'Dodatkowy Opis Strefy'} source={'description'} choices={courtSource} />
       <SelectArrayInput source={'date'} label={'Dostępność'} choices={availableDays} optionText={optionRenderer} />
+      <NumberInput defaultValue={90} label={'Czas rezerwacji'} source={'sessionTime'} />
       <NumberInput min={0} label={'Mecze, Turnieje'} source={'classes_and_sports_training'} />
       <NumberInput min={0} label={'Zajęcia Sportowe Treningi'} source={'tournament_matches'} />
       <NumberInput min={0} label={'Uczelniany Klub AZS UAM'} source={'university_club'} />
